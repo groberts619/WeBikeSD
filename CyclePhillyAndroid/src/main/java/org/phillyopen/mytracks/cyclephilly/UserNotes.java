4e3529fade4682ee0359e3e3e1b3c5a1a435a75f
LAPTOP-PC\groberts619 <glenroberts4life@yahoo.com>
2017-02-03 13:54:18 -0800

    usernotes now displaying ui view with spinner, now working on click event for each spinner item

--- CyclePhillyAndroid/src/main/java/org/phillyopen/mytracks/cyclephilly/UserNotes.java
+++ CyclePhillyAndroid/src/main/java/org/phillyopen/mytracks/cyclephilly/UserNotes.java
@@ -1,10 +1,44 @@
 package org.phillyopen.mytracks.cyclephilly;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import java.util.Map;
 
 /**
  * Created by groberts619 on 1/30/2017.
  */
 
 public class UserNotes extends Activity {
+    public final static int PREF_NOTES = 1;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.usernotes);
+
+        SharedPreferences settings = getSharedPreferences("PREFS", 0);
+        Map<String, ?> prefs = settings.getAll();
+        for (Map.Entry<String, ?> p : prefs.entrySet()) {
+            int key = Integer.parseInt(p.getKey());
+            // CharSequence value = (CharSequence) p.getValue();
+
+            switch (key) {
+                case PREF_NOTES:
+                    ((Spinner) findViewById(R.id.usernotesSpinner))
+                            .setSelection(((Integer) p.getValue()).intValue());
+            }
+        }
+    }
+
+    private void savePreferences() {
+        // Save user preferences. We need an Editor object to
+        // make changes. All objects are from android.context.Context
+        final SharedPreferences settings = getSharedPreferences("PREFS", 0);
+        SharedPreferences.Editor editor = settings.edit();
+
+        editor.putInt("" + PREF_NOTES, ((Spinner) findViewById(R.id.usernotesSpinner))
+                .getSelectedItemPosition());
+    }
 }
